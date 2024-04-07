57fc908cf624d71de9eab1dca5e6ed699aa07c84
Bruno Perez <brunogperez91@gmail.com>
2024-04-07 12:53:55 -0300

    fix chat router

--- src/routes/chat.router.js
+++ src/routes/chat.router.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import {
     getMessages,
     createMessage
-} from '../controllers/chat.controller'
+} from '../controllers/chat.controller.js'
 
 const router = Router()
 
